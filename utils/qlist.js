c5c6a19a99e937821a8ef3c8862b53a7aaba8dfb
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-10-23 15:53:36 +1100

    rearranged questions

--- utils/qlist.js
+++ utils/qlist.js
@@ -1,49 +1,50 @@
 const questions = [
   {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username',
+    type: "input",
+    name: "title",
+    message: "What is the title of this project?",
   },
   {
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address',
+    type: 'editor',
+    name: 'description',
+    message: 'Enter a discription',
   },
   {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of this project?',
+    type: 'editor',
+    name: 'installation',
+    message: 'Enter Installation instructions',
   },
-  // {
-  //   type: 'editor',
-  //   name: 'description',
-  //   message: 'Enter a discription',
-  // },
-  // {
-  //   type: 'editor',
-  //   name: 'installation',
-  //   message: 'Enter Installation instructions',
-  // },
-  // {
-  //   type: 'editor',
-  //   name: 'usage',
-  //   message: 'Enter Usage details',
-  // },
-  // {
-  //   type: 'editor',
-  //   name: 'test',
-  //   message: 'Enter Test Instructions',
-  // },
-  // {
-  //   type: 'editor',
-  //   name: 'contribution',
-  //   message: 'Enter Contribution Guidlines',
-  // },
   {
-    type: 'list',
-    name: 'license',
-    message: 'Choose a license',
-    choices: ['Apache 2.0', 'MIT', 'MPL 2.0', 'The Unlicense', 'WTFPL', 'none']
-  }];
+    type: 'editor',
+    name: 'usage',
+    message: 'Enter how to use the application',
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
+  {
+    type: 'editor',
+    name: 'test',
+    message: 'Enter Test Instructions',
+  },
+  {
+    type: 'editor',
+    name: 'contribution',
+    message: 'Enter Contribution Guidlines',
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license",
+    choices: ["Apache 2.0", "MIT", "MPL 2.0", "The Unlicense", "WTFPL", "none"],
+  },
+];
 
-module.exports = {questions};
+module.exports = { questions };
